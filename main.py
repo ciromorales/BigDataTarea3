d72a8ed77392b023fb5bdfb95dd1de4c887acfb7
cirom <ciro.moralesc@outlook.com>
2024-10-19 17:23:49 -0500

    Big Data Tarea 3
    Actualiza comentarios

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
+#Importa las librerías necesarias: Se importa SparkSession para crear una sesión de Spark y functions como F para
+#utilizar funciones de PySpark en operaciones sobre DataFrames.
 from pyspark.sql import SparkSession, functions as F
 
-# Inicializa la sesión de Spark
+# Crea una sesión de Spark: Se inicializa una nueva sesión de Spark con el nombre "Tarea3".
+# Si ya existe una sesión con ese nombre, se utiliza la existente.
 spark = SparkSession.builder.appName('Tarea3').getOrCreate()
-# Define la ruta del archivo .csv en HDFS
-file_path = 'hdfs://localhost:9000/Tarea3/rows.csv'
 
-# Lee el archivo .csv
+#Define la ruta del archivo CSV: Aquí se especifica la ruta del archivo CSV que se va a cargar.
+#file_path = 'hdfs://localhost:9000/Tarea3/rows.csv'
+file_path = 'LISTADO_COLEGIOS_BOGOTA_20241019.csv'
+
+# Carga el archivo CSV en un DataFrame
 df = spark.read.format('csv').option('header','true').option('inferSchema','true').load(file_path)
 
-#imprimimos el esquema
+#Imprime el esquema del DataFrame
 df.printSchema()
 
-# Muestra las primeras filas del DataFrame
+# Muestra las primeras filas
 df.show()
 
-# Estadisticas básicas
+# Genera estadísticas básicas
 df.summary().show()
 
-#Filtra colegios que tienen más de una sede
+# Filtra colegios con más de una sede
 print("Colegios con más de una sede\n")
 colegios_con_sedes = df.filter(F.col('numero_de_Sedes') > 1).select('nombreestablecimiento', 'numero_de_Sedes',
                                                                     'telefono','correo_Electronico')
 colegios_con_sedes.show()
 
-#Ordena colegios por número de sedes en orden descendente
+# Ordena colegios por número de sedes
 print("Colegios ordenados por número de sedes (mayor a menor)\n")
 sorted_df = df.sort(F.col("numero_de_Sedes").desc())
 sorted_df.show()
